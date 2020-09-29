85847879a718f02e3cbba69d1690b5520a4137ce
Andrew Brown <DaVinci0137@gmail.com>
2020-09-28 20:43:44 -0400

    added Clock

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Welcome from './components/welcome/Welcome';
+import Clock from './components/clock/Clock';
 
 function App() {
   return (
     <div className="App">
       <Welcome name="DaVinci0137" />
+      <Clock />
     </div>
   );
 }
