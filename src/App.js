539121b7d87f699c61a18a796b7674ea62a1da5a
Andrew Brown <DaVinci0137@gmail.com>
2020-09-28 23:07:04 -0400

    modified js and json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router';
 import './App.css';
 import Welcome from './components/welcome/Welcome';
 import Clock from './components/clock/Clock';
+import Contact from './components/contact/Contact';
+import Navigation from './components/navigation/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <Welcome name="DaVinci0137" />
-      <Clock />
-    </div>
+    <Switch>
+      <div className="App">
+        <Navigation />
+        <Route
+          path="/"
+          render={(props) => <Welcome {...props} name="DaVinci0137" />}
+        />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Switch>
   );
 }
 
